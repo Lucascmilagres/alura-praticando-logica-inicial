6e15f82e859929694840ba1a0c6d0e34ad848ed0
Lucas Costa <lucas.costa@crmbonus.com>
2024-05-17 11:47:07 -0300

    Update Alugames

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,18 +1,33 @@
+let jogosAlugados = 0; 
+
+function exibirQuantAlugados(){
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id){
     let selectGame = document.getElementById(`game-${id}`);
     let selectImg = selectGame.querySelector('.dashboard__item__img');
     let selectButton = selectGame.querySelector('.dashboard__item__button');
-    let selectText = selectGame.querySelector('a');
+    let selectName = selectGame.querySelector('p').innerText;
     if (selectImg.classList.contains('dashboard__item__img--rented')){
-        selectImg.classList.remove('dashboard__item__img--rented');
-        selectButton.classList.remove('dashboard__item__button--return');
-        selectText.innerText = 'Alugar';
-        return;
+        if(confirm(`Você realmente deseja devolver o jogo ${selectName}`)){
+            selectImg.classList.remove('dashboard__item__img--rented');
+            selectButton.classList.remove('dashboard__item__button--return');
+            selectButton.innerText = 'Alugar';
+            jogosAlugados--;
+        }
     }
-    if (selectImg.classList.contains('dashboard__item__img')){
-        selectImg.classList.add('dashboard__item__img--rented');
-        selectButton.classList.add('dashboard__item__button--return');
-        selectText.innerText = 'Devolver';
-        return;
+    else {
+            selectImg.classList.add('dashboard__item__img--rented');
+            selectButton.classList.add('dashboard__item__button--return');
+            selectButton.innerText = 'Devolver';
+            jogosAlugados++;
     }
+    exibirQuantAlugados();
 }
+
+// Inicializa a contagem caso os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    exibirQuantAlugados();
+});
